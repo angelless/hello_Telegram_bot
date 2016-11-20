2d49840b34bcc760916aca9b7a8cc023ff0047c6
angelless <dongkyu91622@gmail.com>
2016-11-21 01:05:52 +0900

    Create myStatCommand.java

---
+++ src/main/java/commands/myStatCommand.java
@@ -0,0 +1,35 @@
+package commands;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.AbsSender;
+import org.telegram.telegrambots.bots.commands.BotCommand;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.logging.BotLogger;
+
+public class myStatCommand extends BotCommand {
+
+	public myStatCommand() {
+		super("Hello", "say Hello");
+
+	}
+
+	@Override
+	public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+		String m = "hello <b>";
+		SendMessage message = new SendMessage();
+		message.setChatId(chat.getId().toString());
+		message.enableHtml(true);
+		message.setText(m + user.getUserName() + "</b>");
+		try {
+			absSender.sendMessage(message);
+		} catch (TelegramApiException e) {
+
+			BotLogger.error("MAIN", e);
+		}
+
+	}
+
+}
